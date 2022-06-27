458d8fedc404ca200b9871ed0357c54080d01617
Hiram Guerrero <hiramg90@gmail.com>
2022-06-26 20:54:35 -0500

    section 14 completed: Noteballs: Bulma & Design

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <router-link to="/">Notes</router-link> | 
-  <router-link to="/stats">Stats</router-link>
-  <router-view>
+  <!--  <router-link to="/" class="button">Notes</router-link>
+    |
+    <router-link to="/stats" class="button">Stats</router-link>-->
+  <NavBar/>
 
-  </router-view>
+  <div class="container is-max-desktop px-2 py-4">
+    <router-view/>
+  </div>
+
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
+
+<script setup>
+import NavBar from '@/components/Layout/NavBar.vue';
+</script>
