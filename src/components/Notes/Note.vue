c4c64eedd577aa2a687cdfc509400fe07079d90d
Hiram Guerrero <hiramg90@gmail.com>
2022-07-04 17:46:33 -0500

    Section 18 Noteballs: Delete modal

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -10,15 +10,22 @@
     </div>
     <footer class="card-footer">
       <router-link :to="`/editNote/${note.id}`" class="card-footer-item">Edit</router-link>
+      <!--         @click.prevent="storeNotes.deleteNote(note.id)"-->
       <a href="#" class="card-footer-item"
-         @click.prevent="storeNotes.deleteNote(note.id)">Delete</a>
+         @click.prevent="deleteNote"
+      >Delete</a>
     </footer>
+    <ModalDeleteNote
+        :noteId="note.id"
+        v-if="modals.deleteNote"
+        v-model="modals.deleteNote"/>
   </div>
 </template>
 
 <script setup>
-import {computed} from 'vue';
+import {computed, reactive} from 'vue';
 import {useStoreNotes} from "../../stores/storeNote";
+import ModalDeleteNote from "./ModalDeleteNote.vue";
 
 const storeNotes = useStoreNotes();
 const props = defineProps({
@@ -32,6 +39,15 @@ const characterLength = computed(() => {
   let description = props.note.content.length > 1 ? 'characters' : 'character';
   return `${props.note.content.length} ${description}`;
 });
+
+const modals = reactive({
+  deleteNote: false,
+  editNote: false,
+});
+
+const deleteNote = () => {
+  modals.deleteNote = true;
+};
 </script>
 
 <style scoped>
